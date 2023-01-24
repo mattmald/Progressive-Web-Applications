47b2bea7c2ca7b923ee8771d6585c7eed6ac1be8
mattmald <Mat.maldonado012@gmail.com>
2023-01-23 18:07:08 -0600

    added logic still needs webpack-config file

---
+++ client/src/js/database.js
@@ -0,0 +1,48 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transction('contact', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transction('contact', 'readonly');
+
+  const store = tx.objectStore('contact');
+  
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+initdb();
